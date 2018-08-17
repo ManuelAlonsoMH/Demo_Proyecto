8ba0eae54fa3e00c9357265578b1a59933a453a9
ManuelAlonsoMH <39062329+ManuelAlonsoMH@users.noreply.github.com>
2018-08-17 07:47:50 -0500

    Subi mi pagina web

---
+++ proyecto_web/busqueda/js/jquery.ticker.js
@@ -0,0 +1,18 @@
+﻿/**
+ * @author: Ricardo Antonio 
+ */
+
+var search = document.getElementById("search"),
+    food = document.getElementsByTagName("span"),
+    forEach = Array.prototype.forEach;
+
+search.addEventListener("keyup", function(e){
+    var choice = this.value;
+  
+    forEach.call(food, function(f){
+        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+            f.parentNode.style.display = "none";        
+        else
+            f.parentNode.style.display = "block";        
+    });
+}, false);
